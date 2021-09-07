d1b4385af3c4eb7921ad568ed222013ef10fa8be
唐涛 <wb-tt861376@cainiao.com>
2021-09-07 11:04:55 +0800

    Function与lamda表达式优化

--- src/test/java/com/springboottemplate/java8/FunctionTest.java
+++ src/test/java/com/springboottemplate/java8/FunctionTest.java
@@ -1,8 +1,10 @@
 package com.springboottemplate.java8;
 
 import org.apache.poi.ss.formula.functions.T;
+import org.apache.poi.ss.formula.functions.Value;
 import org.junit.Test;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
@@ -12,6 +14,7 @@ import java.util.function.Function;
  * @description: TODO
  * @date 2021/6/3 10:24
  */
+
 public class FunctionTest {
 
     public static void main(String[] args) {
@@ -20,44 +23,84 @@ public class FunctionTest {
     }
 
 
+    /**
+     * function在同一个方法中的使用
+     */
     @Test
     public void test() {
 
-        testFunction("张三", (string) -> {
-            System.out.println("*******2*********");
-            List list = modifyString(string);
-            return list;
+        Function<String, List<String>> function1 = new Function<String, List<String>>() {
+            @Override
+            public List<String> apply(String s) {
+                System.out.println("********1**********");
+                System.out.println(s);
+                return null;
+            }
+        };
+
+        //这是上面代码的简写
+        Function<String, List<String>> function3 = (value) -> {
+            System.out.println("********2**********");
+            System.out.println(value);
+            return null;
+        };
+
+        //这是上面代码的简写,yige参数，括号可以省略
+        Function<String, List<String>> function2 = value -> {
+            System.out.println("********2**********");
+            System.out.println(value);
+            return null;
+        };
+
+
+        //只有调用了apply,上面的function代码才会执行,其他的不要考虑那么多
+        function1.apply("zhangsan");
+        function2.apply("lisi");
+    }
+
+
+    /**
+     * 下面两个方法表示function在不同方法中的应用，不推荐这样使用，跟放在同一个方法中差不多，而且代码理解起来费劲，吃力不讨好
+     * <p>
+     * <p>
+     * <p>
+     * String 传入的是name
+     * <p>
+     * nameList 指的是name的集合
+     *
+     * @param nameList
+     */
+    public List doSomeing(Function<String, List<T>> nameList) {
+        System.out.println("*****第一步****");
+        String name = "zhangsan";
+        if (true) {
+            name = "zhangsan";
+        }
+        System.out.println("*****第二步****");
+        List<T> dataList = nameList.apply(name);
+        System.out.println("*****第四步****");
+        //执行 nameList.apply(name);，相当于直接执行下面代码，
+        return dataList;
+    }
+
+
+    @Test
+    public void callDoSomeing() {
+        List list = doSomeing(new Function<String, List<T>>() {
+            @Override
+            public List<T> apply(String s) {
+                System.out.println("*****第三步****");
+                List objects = new ArrayList<>();
+                objects.add("lisi");
+                objects.add(s);
+                return objects;
+            }
+
         });
+        System.out.println("*****第五步****");
+        System.out.println(list.toString());
     }
 
-    public void testFunction(String name, Function<String, List<T>> getList) {
-        System.out.println("*******1*********");
-        /**
-         * 1、执行  getList.apply(name)
-         * 相当于
-         *  执行下面的内容
-         *       (string) -> {
-         *             List list = modifyString(string);
-         *             return list;
-         *         });
-         *  2、 为什么string变成了张三
-         *   因为执行 getList.apply(name)等于执行
-         *     执行下面的内容
-         *       (string) -> {
-         *             List list = modifyString(string);
-         *             return list;
-         *         });
-         *   那么name就传递给 (string)了
-         */
-        System.out.println(getList.apply(name));
-    }
-
-
-    public List modifyString(String a) {
-        System.out.println("*******3*********");
-        ArrayList objects = new ArrayList<>();
-        objects.add("lisi");
-        objects.add(a);
-        return objects;
-    }
 }
+
+
--- src/test/java/com/springboottemplate/java8/lamdaTest.java
+++ src/test/java/com/springboottemplate/java8/lamdaTest.java
@@ -3,6 +3,7 @@ package com.springboottemplate.java8;
 import com.google.common.collect.Lists;
 import com.springboottemplate.pojo.User;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.ss.formula.functions.T;
 import org.junit.Test;
 
 import java.util.*;
@@ -14,14 +15,24 @@ public class lamdaTest {
 
     /**
      * lambda表达式用得最多的场合就是替代匿名内部类，而实现Runnable接口是匿名内部类的经典例子
+     * <p>
+     * <p>
+     * <p>
+     * Lambda 表达式的结构：
+     * <p>
+     * Lambda 表达式可以具有零个，一个或多个参数。
+     * 可以显式声明参数的类型，也可以由编译器自动从上下文推断参数的类型。例如 (int a) 与刚才相同 (a)。
+     * 参数用小括号括起来，用逗号分隔。例如 (a, b) 或 (int a, int b) 或 (String a, int b, float c)。
+     * 空括号用于表示一组空的参数。例如 () -> 42。
+     * 当有且仅有一个参数时，如果不显式指明类型，则不必使用小括号。例如 a -> return a*a。
+     * Lambda 表达式的正文可以包含零条，一条或多条语句。
+     * 如果 Lambda 表达式的正文只有一条语句，则大括号可不用写，且表达式的返回值类型要与匿名函数的返回类型相同。
+     * 如果 Lambda 表达式的正文有一条以上的语句必须包含在大括号（代码块）中，且表达式的返回值类型要与匿名函数的返回类型相同。
      */
     @Test
     public void InnerClassTest() {
-        new Thread(() -> System.out.println("It's a lambda function!")).start();
 
-        /**
-         *等效于
-         */
+
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -29,11 +40,22 @@ public class lamdaTest {
             }
         }).start();
 
+         // 空括号用于表示一组空的参数。例如下面
+        //上面代码的简写(   () -> 这个（）代表的是接口的参数，因为Runnable就一个接口，且这个接口没有参数，所以可以直接简写为（）      )
+        new Thread(() -> System.out.println("It's a lambda function!")).start();
+
+
+        Thread thread = new Thread(() -> {
+            System.out.println("The old runable now is using!");
+        });
+        thread.start();
+
+
         /**
          * 集合比较
          */
         List<String> names = Arrays.asList("peter", "anna", "mike", "xenia");
-        List<String> finalNames=names.stream().collect(Collectors.toList());
+        List<String> finalNames = names.stream().collect(Collectors.toList());
         int i = 1;
         boolean success = false;
         AtomicBoolean needSendMsg = new AtomicBoolean(false);
